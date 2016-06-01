723705616cfd35b9654c643262641325ab1d7602
kill0rz <kill0rz@users.noreply.github.com>
2016-06-01 14:15:12 +0200

    v1.1

--- www/pflichtpost.php
+++ www/pflichtpost.php
@@ -1,11 +1,11 @@
 <?php
 
-require('./global.php');
-require('./acp/lib/class_parse.php');
-require('./acp/lib/class_parsecode.php');	
-require('./acp/lib/options.inc.php');	
+require './global.php';
+require './acp/lib/class_parse.php';
+require './acp/lib/class_parsecode.php';
+require './acp/lib/options.inc.php';
 
-if(!($wbbuserdata['userid'] == 1)){
+if (!($wbbuserdata['userid'] == 1)) {
 	header("Location: ./index.php");
 	exit();
 }
@@ -18,13 +18,13 @@ $usercounter = 0;
 Post-ID: <input type="text" name="postidtoinsert" value="<?php echo $_POST['postidtoinsert']; ?>" /><br><br><br>
 <?php
 
-	$result = $db->unbuffered_query("SELECT userid,username FROM bb".$n."_users");
-	while ($row = $db->fetch_array($result)){
-		$optionstring .= "<option value='{$row['userid']}'>{$row['userid']} - {$row['username']}</option>";
-		$usercounter++;
-	}
+$result = $db->unbuffered_query("SELECT userid,username FROM bb1_users");
+while ($row = $db->fetch_array($result)) {
+	$optionstring .= "<option value='{$row['userid']}'>{$row['userid']} - " . htmlentities($row['username'], ENT_NOQUOTES | ENT_HTML401, 'ISO-8859-1') . "</option>";
+	$usercounter++;
+}
 
-?>      
+?>
 User, die den Post sehen sollen (Mehrfachauswahl mit Strg-Taste):<br>
 <select name="users[]" size="<?php echo $usercounter; ?>" multiple>
 <?php echo $optionstring; ?>
@@ -35,45 +35,50 @@ User, die den Post sehen sollen (Mehrfachauswahl mit Strg-Taste):<br>
 
 <?php
 
-if(isset($_POST['postidtoinsert']) and trim($_POST['postidtoinsert']) != ''){
+if (isset($_POST['postidtoinsert']) and trim($_POST['postidtoinsert']) != '') {
 	$postidtoinsert = intval(trim($_POST['postidtoinsert']));
 	$postidexists = false;
 
-	$result = $db->unbuffered_query("SELECT * FROM bb".$n."_posts WHERE postid = '$postidtoinsert'");
-	while ($row = $db->fetch_array($result)){
-		if(isset($row['postid'])){
+	$result = $db->unbuffered_query("SELECT * FROM bb" . $n . "_posts WHERE postid = '$postidtoinsert'");
+	while ($row = $db->fetch_array($result)) {
+		if (isset($row['postid'])) {
 			$postidexists = true;
 		}
 	}
-	
+
 	$userids = array();
-	$result = $db->unbuffered_query("SELECT userid FROM bb".$n."_users");
-	while ($row = $db->fetch_array($result)){
+	$result = $db->unbuffered_query("SELECT userid FROM bb" . $n . "_users");
+	while ($row = $db->fetch_array($result)) {
 		$userids[] = $row['userid'];
 	}
 
 	$userstoinsert = $_POST['users'];
-	
-	if($postidexists){
+
+	if ($postidexists) {
 		$zaehler_true = 0;
 		$zaehler_false = 0;
-		foreach($userstoinsert as $userid){
+		foreach ($userstoinsert as $userid) {
 			$dorun = false;
-			$result = $db->unbuffered_query("SELECT * FROM bb".$n."_users");
-			while ($row = $db->fetch_array($result)){
-				if($userid == $row['userid']) $dorun = true;
+			$result = $db->unbuffered_query("SELECT * FROM bb" . $n . "_users");
+			while ($row = $db->fetch_array($result)) {
+				if ($userid == $row['userid']) {
+					$dorun = true;
+				}
+
 			}
-			if($dorun){
-				$db->unbuffered_query("INSERT INTO bb".$n."_pflichtpost (userid, postID) VALUES ('".$userid."','".$postidtoinsert."')");
-				echo "<font color=green>UserID ".$userid." erfolgreich eingetragen!</font><br>";
+			if ($dorun) {
+				$db->unbuffered_query("INSERT INTO bb" . $n . "_pflichtpost (userid, postID) VALUES ('" . $userid . "','" . $postidtoinsert . "')");
+				echo "<font color=green>UserID " . $userid . " erfolgreich eingetragen!</font><br>";
 				$zaehler_true++;
-			}else{
-				echo "<font color=red>UserID ".$userid." existiert nicht!</font><br>";
+			} else {
+				echo "<font color=red>UserID " . $userid . " existiert nicht!</font><br>";
 				$zaehler_false++;
 			}
 		}
-		
-		echo "<br><font color=green>".$zaehler_true."</font> User wurden erfolgreich eingetragen und bei <font color=red>".$zaehler_false."</font> Usern gab es einen Fehler.";
-	}else echo "Den Post mit der ID ".$postidtoinsert." gibt es nicht!";
-	
+
+		echo "<br><font color=green>" . $zaehler_true . "</font> User wurden erfolgreich eingetragen und bei <font color=red>" . $zaehler_false . "</font> Usern gab es einen Fehler.";
+	} else {
+		echo "Den Post mit der ID " . $postidtoinsert . " gibt es nicht!";
+	}
+
 }
